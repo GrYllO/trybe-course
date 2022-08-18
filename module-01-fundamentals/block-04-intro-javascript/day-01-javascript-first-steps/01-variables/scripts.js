66552960a027948ef81c3620279bc28611687573
Felipe "GrYllO <felipe.grilo.sousa@gmail.com>
2022-08-18 20:07:54 -0300

    moved Exercise to the right folder;

--- module-01-fundamentals/block-04-intro-javascript/day-01-javascript-first-steps/01-variables/scripts.js
+++ module-01-fundamentals/block-04-intro-javascript/day-01-javascript-first-steps/01-variables/scripts.js
@@ -1,10 +1,12 @@
 const myName = "Big Smoke";
 const birthCity = "Los Santos, San Andreas";
 let birthYear = 1958;
-console.log("I'm " + myName + ", from " + birthCity + ", born @ " + birthYear + ".");
+console.log(
+	"I'm " + myName + ", from " + birthCity + ", born @ " + birthYear + "."
+);
 
 birthYear = 2030;
 console.log(birthYear);
 
-birthCity = "San Fierro"
+birthCity = "San Fierro";
 console.log(birthCity);
