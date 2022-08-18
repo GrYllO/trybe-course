35d5048dc235c5e94b6fcf3655692992d0ef802e
Felipe "GrYllO <felipe.grilo.sousa@gmail.com>
2022-08-18 18:57:20 -0300

    added First exercise;

---
+++ module-01-fundamentals/block-04-intro-javascript/day-01-javascript-first-steps/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Big Smoke";
+const birthCity = "Los Santos, San Andreas";
+let birthYear = 1958;
+console.log("I'm " + myName + ", from " + birthCity + ", born @ " + birthYear + ".");
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "San Fierro"
+console.log(birthCity);
