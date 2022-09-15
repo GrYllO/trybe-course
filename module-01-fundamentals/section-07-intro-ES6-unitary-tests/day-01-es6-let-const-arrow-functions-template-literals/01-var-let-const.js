4a656779e453a0a6578694bb301d510df0b2795a
GrYllO <felipe.grilo.sousa@gmail.com>
2022-09-15 16:03:59 -0300

    added Tests;

--- module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-01-es6-let-const-arrow-functions-template-literals/01-var-let-const.js
+++ module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-01-es6-let-const-arrow-functions-template-literals/01-var-let-const.js
@@ -1,15 +1,34 @@
 // Test 01 - simply run the code
-// function userInfo() {
-//   let userEmail = 'maria@email.com';
+/* function userInfo() {
+  let userEmail = 'maria@email.com';
 
-//   // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
-//   console.log(userEmail);
-// }
-// userInfo()
+  // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+  console.log(userEmail);
+}
+userInfo() */
 
 // Test 02 - move console.log outside the function and run it
-function userInfo() {
+/* function userInfo() {
   let userEmail = 'maria@email.com';
+
+  // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+}
+console.log(userEmail);
+userInfo() */
+
+// Test 03 - swap let for var and check if its possible to access the variable
+/* function userInfo() {
+  var userEmail = 'maria@email.com';
+
+  // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+}
+console.log(userEmail);
+userInfo()
+ */
+
+// Test 04 - swap var for const and check if its possible to access the variable
+function userInfo() {
+  const userEmail = 'maria@email.com';
 
   // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
 }
