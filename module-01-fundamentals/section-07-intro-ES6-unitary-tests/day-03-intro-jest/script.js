a2163b7b710050e6bf020acab1edd3b097cac0d8
GrYllO <felipe.grilo.sousa@gmail.com>
2022-09-19 17:38:43 -0300

    added Validation functions;

--- module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-03-intro-jest/script.js
+++ module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-03-intro-jest/script.js
@@ -2,6 +2,18 @@ function sum(a, b) {
 	return a + b;
 }
 
+const isValueNull = (value) => {
+	if (value === '') {
+		throw new Error('summationOf needs to receive a value');
+	}
+};
+
+const isNumber = (number) => {
+	if (Number.isNaN(number)) {
+		throw new Error('summationOf must receive only numbers');
+	}
+};
+
 // summationOf soma todos os números até o parâmetro
 // summationOf(1) -> 1
 // summationOf(3) -> 6
