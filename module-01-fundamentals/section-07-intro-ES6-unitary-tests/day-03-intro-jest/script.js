cc1a850c85fb42a47570add5bd03f81f08eaae9d
GrYllO <felipe.grilo.sousa@gmail.com>
2022-09-19 17:22:00 -0300

    added Tests to summationOfl

--- module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-03-intro-jest/script.js
+++ module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-03-intro-jest/script.js
@@ -2,7 +2,12 @@ function sum(a, b) {
 	return a + b;
 }
 
-function summationOf(){
+// summationOf soma todos os números até o parâmetro
+// summationOf(1) -> 1
+// summationOf(3) -> 6
+// summationOf(5) -> 15
+function summationOf(num){
+	let summation = 0;
 
 }
 
--- module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-03-intro-jest/script.test.js
+++ module-01-fundamentals/section-07-intro-ES6-unitary-tests/day-03-intro-jest/script.test.js
@@ -1,30 +1,32 @@
 const {sum, summationOf} = require('./script');
 
-// Testa se 1 + 2 = 3
+// sum function tests
 test('adds 1 + 2 to equal 3', () => {
 	expect(sum(1,2)).toBe(3);
 });
-
-// Testa se 5.1 + 6.55 = 11.65
 test('adds 5.1 + 6.55 to equal 11.65', () => {
 	expect(sum(5.1,6.55)).toBeCloseTo(11.65);
 });
-
-// Testa se 0 + 0 = 0
 test('adds 0 + 0 to equal 0', () => {
 	expect(sum(0,0)).toBe(0);
 });
-
-// Testa se -5 + -39 = -44
 test('adds -5 + -39 to equal -44', () => {
 	expect(sum(-5,-39)).toBe(-44);
 });
-
-// Testa se 0 + -10 = -10
 test('adds 0 + -10 to equal -10', () => {
 	expect(sum(0,-10)).toBe(-10);
 });
 
+// summationOf function tests
 test('summationOf is a function', () => {
 	expect(typeof summationOf).toBe('function');
+});
+test('summationOf(1) to be 1', () => {
+	expect(summationOf(1)).toBe(1);
+});
+test('summationOf(3) to be 6', () => {
+	expect(summationOf(3)).toBe(6);
+});
+test('summationOf(5) to be 15', () => {
+	expect(summationOf(5)).toBe(15);
 });
