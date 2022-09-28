55eea634d37a1ae7d033c8f15090af1fdf202580
GrYllO <felipe.grilo.sousa@gmail.com>
2022-09-28 15:54:17 -0300

    added External script file;

---
+++ module-01-fundamentals/block-05-javascript-dom-events-and-web-storage/day-01-dom-and-selectors/practice-exercise-01.js
@@ -0,0 +1,4 @@
+const title = document.getElementById('page-title');
+const subtitle = document.getElementById('subtitle');
+const paragraph = document.getElementById("paragraph");
+const secondParagraph = document.getElementById('second-paragraph');
