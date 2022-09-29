75add4cf3a825688b85e1755131fc23a30cfc75c
GrYllO <felipe.grilo.sousa@gmail.com>
2022-09-29 19:04:22 -0300

    fix Formatting with Prettier;

--- module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-01-dom-and-selectors/day-02-js-working-with-elements/exercize-01.js
+++ module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-01-dom-and-selectors/day-02-js-working-with-elements/exercize-01.js
@@ -1,15 +1,15 @@
 // Acesse o elemento elementoOndeVoceEsta.
-const elementWhereYouAre = document.getElementById('elementoOndeVoceEsta');
+const elementWhereYouAre = document.getElementById("elementoOndeVoceEsta");
 
 // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-elementWhereYouAre.parentElement.style.color = 'red';
+elementWhereYouAre.parentElement.style.color = "red";
 
 // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-const firstChildElement = document.getElementById('primeiroFilhoDoFilho');
-firstChildElement.innerText =  'imagine um texto bem legal aqui';
+const firstChildElement = document.getElementById("primeiroFilhoDoFilho");
+firstChildElement.innerText = "imagine um texto bem legal aqui";
 
 // Acesse o primeiroFilho a partir de pai.
-const father = document.getElementById('pai');
+const father = document.getElementById("pai");
 father.firstElementChild;
 
 // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
