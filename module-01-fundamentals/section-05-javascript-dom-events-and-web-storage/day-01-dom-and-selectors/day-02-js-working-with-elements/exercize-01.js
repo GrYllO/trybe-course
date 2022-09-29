9ffb5f6459cbf6bb76399c529a0f146897bcb79f
GrYllO <felipe.grilo.sousa@gmail.com>
2022-09-29 18:49:57 -0300

    added Exercises (WiP);

--- module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-01-dom-and-selectors/day-02-js-working-with-elements/exercize-01.js
+++ module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-01-dom-and-selectors/day-02-js-working-with-elements/exercize-01.js
@@ -1,3 +1,20 @@
+// Acesse o elemento elementoOndeVoceEsta.
 const elementWhereYouAre = document.getElementById('elementoOndeVoceEsta');
 
-console.log(elementWhereYouAre);
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+elementWhereYouAre.parentElement.style.color = 'red';
+
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+const firstChildElement = document.getElementById('primeiroFilhoDoFilho');
+firstChildElement.innerText =  'imagine um texto bem legal aqui';
+
+// Acesse o primeiroFilho a partir de pai.
+const father = document.getElementById('pai');
+father.firstElementChild;
+
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+elementWhereYouAre.previousElementSibling;
+
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de pai.
