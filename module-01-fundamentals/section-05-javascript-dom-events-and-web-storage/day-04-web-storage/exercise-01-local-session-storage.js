7547dc0de5fc35f2f58a098efb287d029d030e7f
GrYllO <felipe.grilo.sousa@gmail.com>
2022-10-03 15:44:21 -0300

    added Files;

--- module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-04-web-storage/exercise-01-local-session-storage.js
+++ module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-04-web-storage/exercise-01-local-session-storage.js
@@ -1,19 +1,54 @@
-console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
-localStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
-localStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
-console.log(localStorage.getItem('lastname')); // retorna o valor 'Smith'
-console.log(localStorage.length); // possui duas entradas, então o retorno é o valor: 2
-localStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
-console.log(localStorage.length); // possui uma entrada, então o retorno é o valor: 1
-localStorage.clear(); // limpa todas as entradas salvas em localStorage
-console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+/* LOCAL STORAGE */
+// não possui nada salvo, então o retorno é o valor: 0
+console.log(localStorage.length);
+// salva uma entrada com a key = 'firstname' e value = 'Adam'
+localStorage.setItem('firstname', 'Adam');
 
-console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
-sessionStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
-sessionStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
-console.log(sessionStorage.getItem('lastname')); // retorna o valor 'Smith'
-console.log(sessionStorage.length); // possui duas entradas, então o retorno é o valor: 2
-sessionStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
-console.log(sessionStorage.length); // possui uma entrada, então o retorno é o valor: 1
-sessionStorage.clear(); // limpa todas as entradas salvas em sessionStorage
-console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+// salva uma entrada com a key = 'lastname' e value = 'Smith'
+localStorage.setItem('lastname', 'Smith');
+
+// retorna o valor 'Smith'
+console.log(localStorage.getItem('lastname'));
+
+// possui duas entradas, então o retorno é o valor: 2
+console.log(localStorage.length);
+
+// remove a entrada referente a key = 'lastname'
+localStorage.removeItem('lastname');
+
+// possui uma entrada, então o retorno é o valor: 1
+console.log(localStorage.length);
+
+// limpa todas as entradas salvas em localStorage
+localStorage.clear();
+
+// não possui nada salvo, então o retorno é o valor: 0
+console.log(localStorage.length);
+
+/* SESSION STORAGE */
+// não possui nada salvo, então o retorno é o valor: 0
+console.log(sessionStorage.length);
+
+// salva uma entrada com a key = 'firstname' e value = 'Adam'
+sessionStorage.setItem('firstname', 'Adam');
+
+// salva uma entrada com a key = 'lastname' e value = 'Smith'
+sessionStorage.setItem('lastname', 'Smith');
+
+// retorna o valor 'Smith'
+console.log(sessionStorage.getItem('lastname'));
+
+// possui duas entradas, então o retorno é o valor: 2
+console.log(sessionStorage.length);
+
+// remove a entrada referente a key = 'lastname'
+sessionStorage.removeItem('lastname');
+
+// possui uma entrada, então o retorno é o valor: 1
+console.log(sessionStorage.length);
+
+// limpa todas as entradas salvas em sessionStorage
+sessionStorage.clear();
+
+// não possui nada salvo, então o retorno é o valor: 0
+console.log(sessionStorage.length);
