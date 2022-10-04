8bf72863918ba195f3e4060d87f2aac2abdd17b0
GrYllO <felipe.grilo.sousa@gmail.com>
2022-10-04 03:06:42 -0300

    added My take on solution;

--- module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-02-js-working-with-elements/exercise-03-removing-elements.js
+++ module-01-fundamentals/section-05-javascript-dom-events-and-web-storage/day-02-js-working-with-elements/exercise-03-removing-elements.js
@@ -1,2 +1,18 @@
 /* Remova todos os elementos filhos de paiDoPai exceto pai, elementoOndeVoceEsta
 e primeiroFilhoDoFilho. */
+
+const filhosDoAvo = document.querySelectorAll("section");
+const textoAlerta = document.getElementById("warningText");
+
+// Remove todos os filhos exceto os indicados
+for (i = 0; i < filhosDoAvo.length; i += 1) {
+  let filhoDoAvo = filhosDoAvo[i];
+  if (
+    filhoDoAvo.id !== "pai" &&
+    filhoDoAvo.id !== "elementoOndeVoceEsta" &&
+    filhoDoAvo.id !== "primeiroFilhoDoFilho"
+  ) {
+    filhoDoAvo.remove();
+  }
+}
+textoAlerta.innerHTML = "";
