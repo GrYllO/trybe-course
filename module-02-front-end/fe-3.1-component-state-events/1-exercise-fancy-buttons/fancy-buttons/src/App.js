9445a578c49599f217a797e9a70ac068db170a49
GrYllO <gryllo@tibiabr.com>
2022-11-29 15:42:55 -0300

    added This bind logic;

--- module-02-front-end/fe-3.1-component-state-events/1-exercise-fancy-buttons/fancy-buttons/src/App.js
+++ module-02-front-end/fe-3.1-component-state-events/1-exercise-fancy-buttons/fancy-buttons/src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 
-function handleClickOne() {
-  console.log('Fancy Button 01 clicked!');
-}
-function handleClickTwo() {
-  console.log('Fancy Button 02 clicked!');
-}
-function handleClickThree() {
-  console.log('Fancy Button 03 clicked!');
-}
-
 export default class App extends Component {
+  constructor() {
+    super()
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickThree = this.handleClickThree.bind(this);
+  }
+
+  handleClickOne() {
+    console.log(this)
+    console.log('Fancy Button 01 clicked!');
+  }
+  handleClickTwo() {
+    console.log(this)
+    console.log('Fancy Button 02 clicked!');
+  }
+  handleClickThree() {
+    console.log(this)
+    console.log('Fancy Button 03 clicked!');
+  }
   render() {
     return (
       <>
-        <button onClick={handleClickOne}>Fancy Button 01</button>
-        <button onClick={handleClickTwo}>Fancy Button 02</button>
-        <button onClick={handleClickThree}>Fancy Button 03</button>
+        <button onClick={this.handleClickOne}>Fancy Button 01</button>
+        <button onClick={this.handleClickTwo}>Fancy Button 02</button>
+        <button onClick={this.handleClickThree}>Fancy Button 03</button>
       </>
     );
   }
