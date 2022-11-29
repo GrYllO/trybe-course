d1b1cb66b4f4a1e0af49fd9a4b976c3cc4faf50c
GrYllO <gryllo@tibiabr.com>
2022-11-29 15:14:36 -0300

    added Fancy Button clicks basic implementation;

--- module-02-front-end/fe-3.1-component-state-events/1-exercise-fancy-buttons/fancy-buttons/src/App.js
+++ module-02-front-end/fe-3.1-component-state-events/1-exercise-fancy-buttons/fancy-buttons/src/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+function handleClickOne() {
+  console.log('Fancy Button 01 clicked!');
+}
+function handleClickTwo() {
+  console.log('Fancy Button 02 clicked!');
+}
+function handleClickThree() {
+  console.log('Fancy Button 03 clicked!');
+}
 
 export default class App extends Component {
   render() {
     return (
-      <button>App</button>
-    )
+      <>
+        <button onClick={handleClickOne}>Fancy Button 01</button>
+        <button onClick={handleClickTwo}>Fancy Button 02</button>
+        <button onClick={handleClickThree}>Fancy Button 03</button>
+      </>
+    );
   }
 }
