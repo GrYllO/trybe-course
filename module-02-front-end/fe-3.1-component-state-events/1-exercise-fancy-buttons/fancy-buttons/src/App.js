1502f7f5e09c26881a58f17b26194c62bfe52db3
GrYllO <gryllo@tibiabr.com>
2022-11-30 01:12:32 -0300

    added State logic use cases;

--- module-02-front-end/fe-3.1-component-state-events/1-exercise-fancy-buttons/fancy-buttons/src/App.js
+++ module-02-front-end/fe-3.1-component-state-events/1-exercise-fancy-buttons/fancy-buttons/src/App.js
@@ -4,6 +4,11 @@ export default class App extends Component {
   constructor() {
     super()
     this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.state = {
+      buttonOneClicks: 0,
+      buttonTwoClicks: 0,
+      buttonThreeClicks: 0
+    }
   }
 
   // function not bound to this, returning undefined
@@ -14,22 +19,25 @@ export default class App extends Component {
 
   // function bound to this via constructor method
   handleClickTwo() {
-    console.log(this)
+    this.setState({buttonTwoClicks: 1}); // setting new state statically
     console.log('Fancy Button 02 clicked!');
   }
 
-  // function bound to this via arrow function
+  // function bound to this directly via arrow function
   handleClickThree = () => {
-    console.log(this)
+    this.setState((previousClicks, _props) => ({
+      buttonThreeClicks: previousClicks.buttonThreeClicks + 1
+    })); // setting new state dynamically
     console.log('Fancy Button 03 clicked!');
   }
 
   render() {
+    const { buttonOneClicks, buttonTwoClicks, buttonThreeClicks } = this.state;
     return (
       <>
-        <button onClick={this.handleClickOne}>Fancy Button 01</button>
-        <button onClick={this.handleClickTwo}>Fancy Button 02</button>
-        <button onClick={this.handleClickThree}>Fancy Button 03</button>
+        <button onClick={this.handleClickOne}>{buttonOneClicks}</button>
+        <button onClick={this.handleClickTwo}>{buttonTwoClicks}</button>
+        <button onClick={this.handleClickThree}>{buttonThreeClicks}</button>
       </>
     );
   }
