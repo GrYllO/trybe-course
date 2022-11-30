e1d65440914a3ad84f4aac71520d6f81ab474f0b
Felipe ¨GrYllO <GrYllO@users.noreply.github.com>
2022-11-30 15:07:15 -0300

    Feature/fe_3.1 (#12)

---
+++ module-02-front-end/fe-3.1-component-state-events/1-exercise-fancy-buttons/fancy-buttons/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+  }
+
+  // public class fields
+  state = {
+    buttonOneClicks: 0,
+    buttonTwoClicks: 0,
+    buttonThreeClicks: 0
+  }
+
+  // function not bound to this, returning undefined
+  handleClickOne() {
+    console.log(this)
+    console.log('Fancy Button 01 clicked!');
+  }
+
+  // function bound to this via constructor method
+  handleClickTwo() {
+    this.setState({buttonTwoClicks: 1}); // setting new state statically
+    console.log('Fancy Button 02 clicked!');
+  }
+
+  // function bound to this directly via arrow function
+  handleClickThree = () => {
+    this.setState((previousClicks, _props) => ({
+      buttonThreeClicks: previousClicks.buttonThreeClicks + 1
+    })); // setting new state dynamically
+    console.log('Fancy Button 03 clicked!');
+  }
+
+  render() {
+    const { buttonOneClicks, buttonTwoClicks, buttonThreeClicks } = this.state;
+    return (
+      <>
+        <button onClick={this.handleClickOne}>{buttonOneClicks}</button>
+        <button onClick={this.handleClickTwo}>{buttonTwoClicks}</button>
+        <button onClick={this.handleClickThree}>{buttonThreeClicks}</button>
+      </>
+    );
+  }
+}
